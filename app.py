f3472b3ebdb3fd31182ca8b5e95dab03e39dff27
Ubuntu <ubuntu@ip-10-226-195-49.us-west-2.compute.internal>
2014-03-14 02:45:26 +0000

    added threaded

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ def allowed_file(filename):
 
 if __name__ == "__main__":
     #port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80,threaded=True)
 
